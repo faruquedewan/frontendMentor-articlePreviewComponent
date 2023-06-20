cdef6823d39ac1469e8e866b9b00d2d4bc2bdd57
faruquedewan <107942129+faruquedewan@users.noreply.github.com>
2023-06-20 20:32:19 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
-var a = document.querySelectorAll('a');
-var p = document.querySelectorAll('p');
+var button = document.querySelectorAll(".button");
 
-a.forEach(element => {
-    element.addEventListener("click", function(){
-        var className = this.classList[0];
-
-        if (this.classList.contains("selected-a")) {
-            this.classList.remove("selected-a");      
-            p.forEach(item => {
-                if (item.classList.contains(className)){
-                    item.classList.add("hidden-p");
-                }
-            });
-            return;
-        }
-
-        a.forEach(item => {
-            item.classList.remove("selected-a");
-        });
-        this.classList.add("selected-a");
-
-        p.forEach(item => {
-            item.classList.add("hidden-p");
-            if (item.classList.contains(className)){
-                item.classList.remove("hidden-p");
-            }
-        });
+button.forEach(element => {
+    element.addEventListener("click", function (){
+        document.querySelector(".div-absolute").classList.toggle("clicked");
     })
 });
